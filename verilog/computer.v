fd0aaa69f85e2820c16541121f63cead5a09ef76
Austin Payne <austin.payne.01@gmail.com>
2016-10-15 23:39:21 -0600

    Add memController.v and computer.v. computer.v is the project top module and handles connections between memController, vgaDisplay, and the core. Implement memController with 32Kb RAM.

--- verilog/computer.v
+++ verilog/computer.v
@@ -19,7 +19,27 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module computer(
+	input enable,
+	input reset,
+	input ext_clk,
+	output [7:0] color, // rrr_ggg_bb
+	output HSync,
+	output VSync,
+	output on // enable LED
     );
+	 
+	wire [14:0] addr;
+	wire [15:0] data;
+	wire we;
+	wire [15:0] data_out;
+	wire [14:0] vga_addr;
+	wire [15:0] vga_data;
+	 
+	clkBuffer _clkBuffer(ext_clk, clk);
+	
+	memController _memController(enable, reset, clk, addr, data, we, data_out, vga_addr, vga_data);
+	vga_display _vga_display(enable, reset, clk, color, HSync, VSync);
 
-
+	assign on = enable;
+	
 endmodule
--- verilog/memController.v
+++ verilog/memController.v
@@ -22,10 +22,12 @@ module memController(
 	input enable,
 	input reset,
 	input ext_clk,
-	input addr[15:0],
-	input data_in[15:0],
+	input [14:0] addr,
+	input [15:0] data_in,
 	input we, // write-enable, write data_in to addr
-	output data_out[15:0]
+	output [15:0] data_out,
+	input [14:0] vga_addr,
+   output [15:0] vga_data_out
     );
 	 
 	// -------------------------------------------------
@@ -42,19 +44,21 @@ module memController(
 	// -------------------------------------------------
 	
 	// memory area sizes
-	parameter SIZE_TEXT   = 15'd_8192;
-	parameter SIZE_GLYPH  = 15'd_1024;
-	parameter SIZE_INPUT  = 15'd_128;
+	parameter SIZE_TEXT   = 14'd_8192;
+	parameter SIZE_GLYPH  = 14'd_1024;
+	parameter SIZE_INPUT  = 14'd_128;
 	parameter SIZE_OUTPUT = SIZE_INPUT;
-	parameter SIZE_INSTR  = 15'd_10240;
+	parameter SIZE_INSTR  = 14'd_10240;
 	parameter SIZE_STACK  = SIZE_INSTR;
 	// memory area starting address (use size to define)
-	parameter ADDR_TEXT   = 15'd0;
+	parameter ADDR_TEXT   = 14'd0;
 	parameter ADDR_GLYH   = ADDR_TEXT + SIZE_TEXT;
 	parameter ADDR_INPUT  = ADDR_GLYPH + SIZE_GLYPH;
 	parameter ADDR_OUTPUT = ADDR_INPUT + SIZE_INPUT;
 	parameter ADDR_INSTR  = ADDR_OUTPUT + SIZE_OUTPUT;
 	parameter ADDR_STACK  = ADD_INSTR + SIZE_INSTR;
 
-
+	ram32Kb _ram32Kb(ext_clk, we, addr    , data_in, data_out,
+						  ext_clk, 0 , vga_addr, 15'd0  , vga_data_out);
+						  
 endmodule
