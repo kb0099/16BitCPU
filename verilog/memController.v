4e59de33dacbf2b96a3408fcc76723489ce12c0c
Austin Payne <austin.payne.01@gmail.com>
2016-10-20 13:28:02 -0600

    Remove extra parameters

--- verilog/memController.v
+++ verilog/memController.v
@@ -35,10 +35,7 @@ module memController(
 	// -------------------------------------------------
 	// Mem Area Name					Starting Addr	Size
 	// -------------------------------------------------
-	// Stack
 	// Instruction Memory 
-	// I/O: Output
-	// I/O: Input
 	// Glyph Memory					0x2000			256x4
 	// Text Memory (VGA Screen)	0x0				128X64
 	// -------------------------------------------------
@@ -46,17 +43,11 @@ module memController(
 	// memory area sizes
 	parameter SIZE_TEXT   = 14'd_8192;
 	parameter SIZE_GLYPH  = 14'd_1024;
-	parameter SIZE_INPUT  = 14'd_128;
-	parameter SIZE_OUTPUT = SIZE_INPUT;
 	parameter SIZE_INSTR  = 14'd_10240;
-	parameter SIZE_STACK  = SIZE_INSTR;
 	// memory area starting address (use size to define)
 	parameter ADDR_TEXT   = 14'd0;
 	parameter ADDR_GLYPH   = ADDR_TEXT + SIZE_TEXT;
-	parameter ADDR_INPUT  = ADDR_GLYPH + SIZE_GLYPH;
-	parameter ADDR_OUTPUT = ADDR_INPUT + SIZE_INPUT;
 	parameter ADDR_INSTR  = ADDR_OUTPUT + SIZE_OUTPUT;
-	parameter ADDR_STACK  = ADDR_INSTR + SIZE_INSTR;
 
 	ram32Kb _ram32Kb(clk, we   , addr    , data_in, data_out,
 						  clk, 1'b0 , vga_addr,   16'd0, vga_data_out);
