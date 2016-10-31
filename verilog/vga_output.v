cd8d110761b732d1495883d3ab553d9cbe4defb5
Austin Payne <austin.payne.01@gmail.com>
2016-10-30 21:46:56 -0600

    Modify vga to work with memory controller and get both text and glyph in one cycleeach (two cycles total, i.e. made everything possible combinational)

--- verilog/vga_output.v
+++ verilog/vga_output.v
@@ -19,15 +19,14 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module vga_output(
-	input enable,
-	input reset,
-	input clk,
-	input [7:0] color_in, // from pixel_generator
-	input [9:0] pixel_counter, // from vga_counter
-	input [8:0] line_counter, // from vga_counter
+	input 			  enable,
+	input 			  reset,
+	input 	  [7:0] color_in, // from pixel_generator
+	input 	  [9:0] pixel_counter, // from vga_counter
+	input 	  [8:0] line_counter, // from vga_counter
 	output reg [7:0] color, // rrr_ggg_bb
-	output reg HSync,
-	output reg VSync
+	output reg 		  HSync,
+	output reg 		  VSync
     );
 	 
 	initial color = 8'b00000000; // initally set to black
@@ -44,28 +43,30 @@ module vga_output(
 	parameter V_SYNC = V_FRONT + 2;
 	parameter V_BACK = V_SYNC + 33;
 	
-	always@(posedge clk) begin
+	// combinational logic
+	always@(*) begin
 		if (reset || !enable) begin
-			color <= 8'b00000000;
-			HSync <= 1'b1;
-			VSync <= 1'b1;
+			color = 8'b00000000;
+			HSync = 1'b1;
+			VSync = 1'b1;
 		end
-		else begin	
+		else begin
 			if (pixel_counter >= H_FRONT && pixel_counter < H_SYNC)
-				HSync <= 1'b0; // send HSync pulse at end of line
+				HSync = 1'b0; // send HSync pulse at end of line
 			else
-				HSync <= 1'b1; // otherwise don't send HSync pulse
+				HSync = 1'b1; // otherwise don't send HSync pulse
 				
 			if (line_counter >= V_FRONT && line_counter < V_SYNC)
-				VSync <= 1'b0; // send VSynce at the bottom of the screen
+				VSync = 1'b0; // send VSynce at the bottom of the screen
 			else
-				VSync <= 1'b1; // otherwise don't send VSync
+				VSync = 1'b1; // otherwise don't send VSync
 			
 			// start drawing at index 0 of pixel_counter and line_counter
-			if (pixel_counter >= 0 && pixel_counter < H_ACTIVE && line_counter >= 0 && line_counter < V_ACTIVE)
-				color <= color_in; // set color to color coming out of pixel_generator
+			if (pixel_counter >= 0 && pixel_counter < H_ACTIVE && 
+				 line_counter  >= 0 && line_counter  < V_ACTIVE)
+				color = color_in; // set color to color coming out of pixel_generator
 			else
-				color <= 8'b00000000; // drive color to ground when not in active region
+				color = 8'b00000000; // drive color to ground when not in active region
 		end
 	end
  
