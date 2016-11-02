81c66db6c8be3414b25f912404d82277bdea320b
Austin Payne <austin.payne.01@gmail.com>
2016-11-01 18:53:33 -0600

    Add AssemblerFormatException for more detailed error messages (with line numbers, etc.)

---
+++ assem/assembler/src/assem/AssemblerFormatException.java
@@ -0,0 +1,13 @@
+package assem;
+
+public class AssemblerFormatException extends Exception{
+	
+	/**
+	 * Constructor (call super)
+	 * @param message exception message to display
+	 */
+	public AssemblerFormatException(String message) {
+		super(message);
+	}
+
+}
